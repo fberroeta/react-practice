867ad859fa5952ace99dab0e841a50868dd7bd99
Francisco Berroeta <fsberroeta@gmail.com>
2023-04-26 09:36:40 -0400

    Formatted code

--- projects/01-twitter-follow-card/src/App.jsx
+++ projects/01-twitter-follow-card/src/App.jsx
@@ -5,35 +5,35 @@ import TwitterFollowCard from './TwitterFollowCard'
 const avatarService = (nickname) => `https://unavatar.io/${nickname}`
 const users = [
   {
-  id:1,
-  name: 'Olivia Berroeta Chandía',
-  avatar: oliAvatar,
-  isFollowing: false,
-  nickname:'oliuwu'
-},
-{
-  id:2,
-  name:'Homero Simpson',
-  avatar:avatarService('homer-simpson'),
-  isFollowing:true,
-  nickname:'homerosimpson'
-},
-{
-  id:3,
-  name:'Eric Cartman',
-  avatar:avatarService('ericcartman'),
-  isFollowing:true,
-  nickname:'thecoon'
-}
+    id: 1,
+    name: 'Olivia Berroeta Chandía',
+    avatar: oliAvatar,
+    isFollowing: false,
+    nickname: 'oliuwu'
+  },
+  {
+    id: 2,
+    name: 'Homero Simpson',
+    avatar: avatarService('homer-simpson'),
+    isFollowing: true,
+    nickname: 'homerosimpson'
+  },
+  {
+    id: 3,
+    name: 'Eric Cartman',
+    avatar: avatarService('ericcartman'),
+    isFollowing: true,
+    nickname: 'thecoon'
+  }
 ]
 
-function App() {
+function App () {
   return (
     <section className='App'>
-    {users.map(({id,name,avatar,isFollowing, nickname})=><TwitterFollowCard key={id} name={name} avatar={avatar} initialIsFollowing={isFollowing} nickname={nickname}/>)}
+      {users.map(({ id, name, avatar, isFollowing, nickname }) => <TwitterFollowCard key={id} name={name} avatar={avatar} initialIsFollowing={isFollowing} nickname={nickname} />)}
     </section>
-  
-    )
+
+  )
 }
 
 export default App
--- projects/01-twitter-follow-card/src/TwitterFollowCard.jsx
+++ projects/01-twitter-follow-card/src/TwitterFollowCard.jsx
@@ -1,34 +1,33 @@
-import {useState} from 'react'
+import { useState } from 'react'
 
 const TwitterFollowCard = ({ avatar, name, initialIsFollowing, nickname }) => {
+  const [isFollowing, setIsFollowing] = useState(initialIsFollowing)
 
-    const [isFollowing, setIsFollowing] = useState(initialIsFollowing)
-    
-    const handleClick = () => {
-        setIsFollowing(!isFollowing)
-    }
+  const handleClick = () => {
+    setIsFollowing(!isFollowing)
+  }
 
-    const avatarAlt = `Avatar de ${name}`
-    const buttonText = isFollowing ? 'Siguiendo' : 'Seguir'
-    const nicknameFix = `@${nickname}`
-    
-    const buttonClass = isFollowing
+  const avatarAlt = `Avatar de ${name}`
+  const buttonText = isFollowing ? 'Siguiendo' : 'Seguir'
+  const nicknameFix = `@${nickname}`
+
+  const buttonClass = isFollowing
     ? 'tw-followCard-button is-following'
     : 'tw-followCard-button'
 
   return (
-    <article className="tw-followCard">
-      <header className="tw-followCard-header">
-        <img className="tw-followCard-avatar" src={avatar} alt={avatarAlt} />
-        <div className="tw-followCard-info">
+    <article className='tw-followCard'>
+      <header className='tw-followCard-header'>
+        <img className='tw-followCard-avatar' src={avatar} alt={avatarAlt} />
+        <div className='tw-followCard-info'>
           <strong>{name}</strong>
-          <span className="tw-followCard-infoUserName">{nicknameFix}</span>
+          <span className='tw-followCard-infoUserName'>{nicknameFix}</span>
         </div>
       </header>
       <aside>
         <button onClick={handleClick} className={buttonClass}>{buttonText}</button>
       </aside>
     </article>
-  );
-};
-export default TwitterFollowCard;
+  )
+}
+export default TwitterFollowCard
