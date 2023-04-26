7a2dd99fd4a3e88cc7b7769fbeac00a339a4430a
Francisco Berroeta <fsberroeta@gmail.com>
2023-04-26 13:00:06 -0400

    Add mouse pointer tracking listener in useEffect

--- projects/03-mouse-pointer-follower/src/App.jsx
+++ projects/03-mouse-pointer-follower/src/App.jsx
@@ -1,6 +1,41 @@
+import { useState, useEffect } from 'react'
+
 function App () {
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+  const buttonText = enabled ? 'Deactivate' : 'Activate'
+  // useffect with enabled
+  useEffect(() => {
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      setPosition({ x: clientX, y: clientY })
+    }
+    // if enable suscribe listener
+    enabled && window.addEventListener('pointermove', handleMove)
+    // Clean useEffect
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
   return (
-    <h3>Proyecto 3</h3>
+    <main>
+      <p />
+      <div style={{
+        position: 'absolute',
+        backgroundColor: '#09f',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: `translate(${position.x}px, ${position.y}px)`,
+        zoom: 1
+      }}
+      />
+      <button onClick={() => setEnabled(!enabled)}>{buttonText}</button>
+    </main>
   )
 }
 
