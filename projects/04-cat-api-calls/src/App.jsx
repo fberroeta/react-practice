e2dcaf57a2c22ec63cbc65f879d3f5400da7d5b5
Francisco Berroeta <fsberroeta@gmail.com>
2023-04-27 18:57:35 -0400

    Add style

--- projects/04-cat-api-calls/src/App.jsx
+++ projects/04-cat-api-calls/src/App.jsx
@@ -6,6 +6,7 @@ const CAT_IMAGE_API_URL = 'https://cataas.com'
 const App = () => {
   const [fact, setFact] = useState(null)
   const [image, setImage] = useState(null)
+
   useEffect(() => {
     fetch(CAT_FACT_API_URL)
       .then(res => res.json())
@@ -28,9 +29,11 @@ const App = () => {
 
   return (
     <main>
-      <h1>Cat Image App</h1>
+      <h1 style={{ fontFamily: 'cursive' }}>Cat Image App</h1>
+
       {fact && <p>{fact}</p>}
       <img src={`${CAT_IMAGE_API_URL}${image}`} alt={`Image of a cat usin g the three words of the cat fact ${fact}`} />
+
     </main>
   )
 }
