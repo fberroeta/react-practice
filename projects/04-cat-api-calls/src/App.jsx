0a6a67783abac162cd800cc46ea1815cc369bfe5
Francisco Berroeta <fsberroeta@gmail.com>
2023-04-28 14:43:04 -0400

    Add useEffect hooks separation and rendering fix

--- projects/04-cat-api-calls/src/App.jsx
+++ projects/04-cat-api-calls/src/App.jsx
@@ -4,8 +4,8 @@ const CAT_FACT_API_URL = 'https://catfact.ninja/fact'
 const CAT_IMAGE_API_URL = 'https://cataas.com'
 
 const App = () => {
-  const [fact, setFact] = useState(null)
-  const [image, setImage] = useState(null)
+  const [fact, setFact] = useState()
+  const [image, setImage] = useState()
 
   useEffect(() => {
     fetch(CAT_FACT_API_URL)
@@ -13,26 +13,29 @@ const App = () => {
       .then(data => {
         const { fact } = data
         setFact(fact)
-
-        const threeFirstWords = fact.split(' ', 3).join(' ')
-
-        const endpointImageApi = `${CAT_IMAGE_API_URL}/cat/says/${threeFirstWords}?json=true`
-
-        fetch(endpointImageApi)
-          .then(res => res.json())
-          .then(data => {
-            const { url } = data
-            setImage(url)
-          })
       })
   }, [])
+
+  useEffect(() => {
+    if (!fact) return
+
+    const threeFirstWords = fact.split(' ', 3).join(' ')
+    const endpointImageApi = `${CAT_IMAGE_API_URL}/cat/says/${threeFirstWords}?json=true`
+
+    fetch(endpointImageApi)
+      .then(res => res.json())
+      .then(data => {
+        const { url } = data
+        setImage(url)
+      })
+  }, [fact])
 
   return (
     <main>
       <h1 style={{ fontFamily: 'cursive' }}>Cat Image App</h1>
 
       {fact && <p>{fact}</p>}
-      <img src={`${CAT_IMAGE_API_URL}${image}`} alt={`Image of a cat usin g the three words of the cat fact ${fact}`} />
+      {image && <img src={`${CAT_IMAGE_API_URL}${image}`} alt={`Image of a cat using the three words of the cat fact: ${fact}`} />}
 
     </main>
   )
